0f9bc509adfa68d63b916b793ba275ecff578efc
purple <cyberusr.purple@gmail.com>
2022-01-03 20:48:29 +0200

    Accidentally pushed main.py with luajit instead of lua

--- parsers/main.py
+++ parsers/main.py
@@ -10,7 +10,7 @@ def run_lua_parser(proc_path: str, io_operation: int = 1):
     :param pid: The pid to run the parser with.
     """
     while True:
-        subprocess.call(['luajit', f'{os.getcwd()}/lua/data_sources/parsers/python_parser_wrapper.lua', proc_path, str(io_operation)])
+        subprocess.call(['lua', f'{os.getcwd()}/lua/data_sources/parsers/python_parser_wrapper.lua', proc_path, str(io_operation)])
         time.sleep(3)
 
 
@@ -19,7 +19,7 @@ def run_lua_rule(rule_name: str):
     :param rule_name: ...
     """
     while True:
-        subprocess.call(['luajit', '-e', f"dofile'{os.getcwd()}/lua/rules/{rule_name}.lua' run()"])
+        subprocess.call(['lua', '-e', f"dofile'{os.getcwd()}/lua/rules/{rule_name}.lua' run()"])
         # subprocess.call(['lua', f"{os.getcwd()}/lua/rules/{rule_name}.lua"])
         time.sleep(3)
 
